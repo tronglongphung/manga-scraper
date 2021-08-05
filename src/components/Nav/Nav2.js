39f6bdd15524ebcd8d6f75393b21a922fe8f68a3
tronglongphung <tronglongphung@gmail.com>
2021-08-06 01:52:12 +0930

    edit nav ui

--- src/components/Nav/Nav2.js
+++ src/components/Nav/Nav2.js
@@ -9,6 +9,11 @@ const navigation = [
   { name: 'Saved Manga', href: '/saved', current: false },
 ];
 
+const login = [
+  { name: 'Log In', href: '/login' },
+  { name: 'Sign Up', href: '/signup' },
+];
+
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
@@ -78,61 +83,23 @@ export default function Nav() {
       );
     } else {
       return (
-        <Menu as="div" className="ml-3 relative">
-          {({ open }) => (
-            <>
-              <div>
-                <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                  <span className="sr-only">Open user menu</span>
-                  <img
-                    className="h-8 w-8 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </Menu.Button>
-              </div>
-              <Transition
-                show={open}
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
+        <>
+          <div className="flex space-x-4">
+            {login.map((item) => (
+              <Link
+                key={item.name}
+                to={item.href}
+                className={classNames(
+                  item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'px-3 py-2 rounded-md text-sm font-medium'
+                )}
+                aria-current={item.current ? 'page' : undefined}
               >
-                <Menu.Items
-                  static
-                  className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-                >
-                  {/* login */}
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        to="/login"
-                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                      >
-                        Log In
-                      </Link>
-                    )}
-                  </Menu.Item>
-
-                  {/* signup */}
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        to="/signup"
-                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                      >
-                        Sign Up
-                      </Link>
-                    )}
-                  </Menu.Item>
-                </Menu.Items>
-              </Transition>
-            </>
-          )}
-        </Menu>
+                {item.name}
+              </Link>
+            ))}
+          </div>
+        </>
       );
     }
   }
