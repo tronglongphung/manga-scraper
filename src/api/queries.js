9f01ab291abb17464fca3907db6ac9bf76cf30e9
tronglongphung <tronglongphung@gmail.com>
2021-08-06 23:50:50 +0930

    modifying queries

--- src/api/queries.js
+++ src/api/queries.js
@@ -11,6 +11,50 @@ export const QUERY_USER = gql`
   }
 `;
 
+export const QUERY_MANGAS = gql`
+  query getMangas($url: String!) {
+    mangaData(url: $url) {
+      name
+      status
+      url
+    }
+  }
+`;
+
+export const QUERY_MANGA_DATA = gql`
+  query getMangaData($url: String!) {
+    mangaData(url: $url) {
+      name
+      status
+    }
+  }
+`;
+
+export const QUERY_MANGA_DETAILS = gql`
+  query getMangaData($key: String!) {
+    manga(key: $key) {
+      name
+      alternative
+      authors
+      status
+      genres
+      rating {
+        ratingFromFive
+        votes
+      }
+      updated
+      description
+      url
+      chapters {
+        chapter
+        views
+        upload_date
+        url
+      }
+    }
+  }
+`;
+
 export const QUERY_CHAPTERS = gql`
   query getChapters($url: String!) {
     chapter(url: $url) {
