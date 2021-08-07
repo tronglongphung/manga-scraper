e816bc307d8ef0c24291a9eded7542355b3d6ff5
tronglongphung <tronglongphung@gmail.com>
2021-08-08 00:33:52 +0930

    comment out unused stuff

--- src/pages/MangaDetails.js
+++ src/pages/MangaDetails.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Route, Link, useParams } from 'react-router-dom';
+// import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_MANGA_DETAILS } from '../api/queries';
 
