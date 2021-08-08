636d2d15d5f6181b829250e67c0b8218c9832452
tronglongphung <tronglongphung@gmail.com>
2021-08-08 15:01:31 +0930

    changed text

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -36,7 +36,6 @@ function Login() {
       <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <img className="mx-auto h-12 w-auto" src="https://img.icons8.com/color/50/000000/hearts.png" alt="Hearts" />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
             <p className="mt-2 text-center text-sm text-gray-600">
               No account?{' '}
