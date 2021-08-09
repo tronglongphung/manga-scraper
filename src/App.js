2588c100d32f81977e450a9c41d6efd77ca0f2a8
tronglongphung <tronglongphung@gmail.com>
2021-08-10 00:37:42 +0930

    adding suspense and lazy

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 import Home from './pages/Home';
 import NoMatch from './pages/NoMatch';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Nav from './components/Nav/';
-import Saved from './pages/Saved';
 import { StoreProvider } from './state/GlobalState';
-import Profile from './pages/Profile';
-import Reader from './pages/Reader';
-import MangaDetails from './pages/MangaDetails';
+import Nav from './components/Nav/';
+
+const Login = lazy(() => import('./pages/Login'));
+const Signup = lazy(() => import('./pages/Signup'));
+const Saved = lazy(() => import('./pages/Saved'));
+const Profile = lazy(() => import('./pages/Profile'));
+const Reader = lazy(() => import('./pages/Reader'));
+const MangaDetails = lazy(() => import('./pages/MangaDetails'));
 
 const httpLink = createHttpLink({
   uri: process.env.REACT_APP_BASEURL + '/graphql',
@@ -42,17 +43,19 @@ function App() {
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route exact path="/manga/:key" children={<MangaDetails />} />
 
-              {/* <Route path="/manga/:mangaKey/:mangaChapterKey" component={Reader} /> */}
+              <Suspense fallback={<div>Loading...</div>}>
+                <Route exact path="/manga/:key" children={<MangaDetails />} />
+                {/* <Route path="/manga/:mangaKey/:mangaChapterKey" component={Reader} /> */}
 
-              {/* remove route reader soon */}
-              <Route exact path="/reader" component={Reader} />
+                {/* remove route reader soon */}
+                <Route exact path="/reader" component={Reader} />
+                <Route exact path="/saved" component={Saved} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/signup" component={Signup} />
+                <Route exact path="/profile" component={Profile} />
+              </Suspense>
 
-              <Route exact path="/saved" component={Saved} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
-              <Route exact path="/profile" component={Profile} />
               <Route component={NoMatch} />
             </Switch>
           </StoreProvider>
