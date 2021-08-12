f0337d7b912b9477971b5b216446410bd458b23d
tronglongphung <tronglongphung@gmail.com>
2021-08-12 13:13:00 +0930

    changing styling attributes

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -6,20 +6,26 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
+import { useStoreContext } from '../../state/GlobalState';
+import FavouriteButton from '../FavouriteButton/FavouriteButton';
 
 const useStyles = makeStyles({
   root: {
     minWidth: 170,
     maxWidth: 170,
-    minHeight: 260,
-    maxHeight: 260,
+    minHeight: 240,
+    maxHeight: 240,
   },
   media: {
-    height: 150,
+    height: 160,
   },
   font: {
     fontSize: '.9rem',
     lineHeight: '1.5',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    bottom: '0px',
   },
   subheading: {
     whiteSpace: 'nowrap',
@@ -41,17 +47,17 @@ const useStyles = makeStyles({
 });
 
 export default function MediaCard(props) {
-  const classes = useStyles();
+  const [state, dispatch] = useStoreContext();
 
+  const classes = useStyles();
   return (
     <>
       <Card className={`${classes.root} overflow-y-scroll m-2`}>
+        <CardMedia className={classes.media} image={props.cover} title={props.name}>
+          <FavouriteButton mangaId={props.url} />
+        </CardMedia>
         <CardActionArea>
-          <Link
-            to={{ pathname: `/manga/${props.url}`, state: { coverImg: `${props.cover}` } }}
-            className={classes.link}
-          >
-            <CardMedia className={classes.media} image={props.cover} title={props.name} />
+          <Link to={`/manga/${props.url}`} className={`${classes.link} hover:text-indigo-600`}>
             <CardContent className={classes.cardcontent}>
               <Typography gutterBottom variant="h6" component="h2" className={classes.font}>
                 {props.name}
